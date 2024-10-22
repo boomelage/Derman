b74e01cf5de62c3616f4ae3e49cc3a2a1fe2cd7e
radusbriciu <radusbriciu@proton.me>
2024-10-22 17:58:48 +0100

    testing

--- build/lib/Derman/Derman.py
+++ build/lib/Derman/Derman.py
@@ -4,28 +4,32 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 def linearize_vol_surface(surface,s,atm_vols):
-    K = np.array(surface.index)
-    T = np.array(atm_vols.index)
-    derman_coefs = pd.Series(np.empty(len(T),dtype=float),index=T)
-    for t in T:
-        vols = surface.loc[:,t].dropna()
-        k = vols.index
-        x = np.array(vols.index)-s
-        y = vols - atm_vols.loc[t]
-        model = LinearRegression(fit_intercept=False)
-        x = x.reshape(-1,1)
-        model.fit(x,y)
-        b = model.coef_[0]
-        derman_coefs.loc[t] = b
-        estimated = pd.Series(np.asarray(k-s)*b + atm_vols.loc[t],index=k)
+    K = np.array(surface.index,dtype=float)
+    T = np.array(atm_vols.index,dtype=int)
+    derman_coefs = {}
+    for t in T.tolist():
+        try:
+            vols = surface.loc[:,t].dropna()
+            k = vols.index
+            x = np.array(vols.index)-s
+            y = vols - atm_vols.loc[t]
+            model = LinearRegression(fit_intercept=False)
+            x = x.reshape(-1,1)
+            model.fit(x,y)
+            b = model.coef_[0]
+            derman_coefs[int(t)] = float(b)
 
-        # plt.figure()
-        # plt.plot(estimated,color='purple', label='estimated')
-        # plt.scatter(vols.index,vols,color = 'green',label='actual')
-        # plt.title(f'{t} day maturity')
-        # plt.ylabel('implied volatility')
-        # plt.xlabel('strike price')
-        # plt.show()
+            # estimated = pd.Series(np.asarray(k-s)*b + atm_vols.loc[t],index=k)
+            # plt.figure()
+            # plt.plot(estimated,color='purple', label='estimated')
+            # plt.scatter(vols.index,vols,color = 'green',label='actual')
+            # plt.title(f'{t} day maturity')
+            # plt.ylabel('implied volatility')
+            # plt.xlabel('strike price')
+            # plt.show()
+        except Exception as e:
+            print(f"error for {t}: {e}")
+            pass
 
     return derman_coefs
 
